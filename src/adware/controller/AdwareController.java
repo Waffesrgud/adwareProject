e6b8f3faaaf2bcb41b33f6bfb848ecb8da469030
Waffesrgud <gcor8853@740s-pl-129.local>
2016-09-27 09:56:11 -0600

    Null doesn't crash it, at the very least.

--- src/adware/controller/AdwareController.java
+++ src/adware/controller/AdwareController.java
@@ -15,20 +15,24 @@ public class AdwareController
 	{
 		String computerBrand = "Toshiba PIX2";
 		int count = 0;
-		while(count < 10)
+		String nullCheck = "yes";
+		while(count < 20 && nullCheck != null)
 		{
 			display.displayMessage("Buy a " + computerBrand + " brand computer!");
 	
 		//Quizzes them on what the best computer brand is, if they get it correct it ends it. 
 		//If they enter anything it adds a point for effort. If they put nothing, it doesn't ever end.
 		String answer = display.collectResponse("What computer is the best?");
+		nullCheck = answer;
+		if (nullCheck != null){
 		System.out.println("The answer was: " + answer);
 		if (answer.equals(computerBrand)){
-			count = 10;
+			count = 20;
 		} else if (!answer.equals("") == true){
 			count++;
 		}
 		System.out.println("They're at " + count + " points~!");
+		}
 		}
 	}
 }
