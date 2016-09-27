392842f5371006f6b62256353ae297ae12829280
Waffesrgud <gcor8853@740s-pl-129.local>
2016-09-27 09:36:18 -0600

    The adware works! The system checks to make sure the answer was not a lazy null or blank answer, and either it succeeds or you get a point for the attempt.

--- src/adware/controller/AdwareController.java
+++ src/adware/controller/AdwareController.java
@@ -13,6 +13,20 @@ public class AdwareController
 	
 	public void start()
 	{
-	 display.displayMessage("Hello, world!");
+		String computerBrand = "Toshiba PIX2";
+		int count = 0;
+		while(count < 10)
+		{
+			display.displayMessage("Buy a " + computerBrand + " brand computer!");
+	
+		//Quizzes them on what the best computer brand is, if they get it correct it ends it. 
+		//If they enter anything it adds a point for effort. If they put nothing, it doesn't ever end.
+		String answer = display.collectResponse("What computer is the best?");
+		if (answer.equals(computerBrand)){
+			count = 10;
+		} else if (answer.equals("")||answer.equals(null) == false){
+			count++;
+		}
+		}
 	}
 }
