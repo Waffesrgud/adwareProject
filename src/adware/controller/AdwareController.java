e1dc5e0ef940eafa1f00b2d8841972ea2c3ca495
Waffesrgud <gcor8853@740s-pl-129.local>
2016-09-29 08:53:57 -0600

    Added some testing stuff, like isDouble or isInteger. They work, as I detailed in start().

--- src/adware/controller/AdwareController.java
+++ src/adware/controller/AdwareController.java
@@ -1,6 +1,7 @@
 package adware.controller;
 
 import java.util.Scanner;
+
 import adware.view.AdwareView;
 
 public class AdwareController
@@ -13,7 +14,7 @@ public class AdwareController
 	
 	public void start()
 	{
-		String computerBrand = "Toshiba PIX2";
+		/* String computerBrand = "Toshiba PIX2";
 		int count = 0;
 		String nullCheck = "yes";
 		while(count < 20 && nullCheck != null)
@@ -31,8 +32,68 @@ public class AdwareController
 		} else if (!answer.equals("") == true){
 			count++;
 		}
+		}
 		System.out.println("They're at " + count + " points~!");
+		
+		
+		}	*/
+		String answer = display.collectResponse("Any number you input is fine, haha. Just make sure it's a number, haha.");
+		if(answer != null && !answer.equals(""))
+		{
+		if(isDouble(answer))
+		{
+			System.out.println("Okay, on the double!");
+		} else {
+			System.out.println("That's sick, and you're sick.");
 		}
+		if(isInteger(answer)){
+			System.out.println("Okay, on the int.");
+		} else {
+			System.out.println("Shut up, nerd boy.");
 		}
+		
+		}
+	}
+	/**
+	 * This program checks if the input is a valid double and if otherwise returns a message indicating so.
+	 * @param potentialValue The input string.
+	 * @return Whether or not it is possible to parse the input, in boolean format.
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+		boolean isParseable = false;
+		
+		try
+		{
+			double test = Double.parseDouble(potentialValue);
+			isParseable = true;
+		}
+		catch(NumberFormatException notDouble)
+		{
+			display.displayMessage("This is NOT a double value, kiddo.");
+		}
+		
+		return isParseable;
+	}
+	/**
+	 * Checks if the input is a valid integer and if otherwise returns a message indicating so.
+	 * @param potentialNumber The input string.
+	 * @return Whether or not it can parse the input, in boolean format.
+	 */
+	private boolean isInteger(String potentialNumber)
+	{
+		boolean isParseable = false;
+		try
+		{
+			int test = Integer.parseInt(potentialNumber);
+			isParseable = true;
+			
+		}
+		catch(NumberFormatException notInt)
+		{
+			display.displayMessage(notInt.getMessage());
+			display.displayMessage("Type in an integer you friccin moron..!");			
+		}
+		return isParseable;
 	}
 }
