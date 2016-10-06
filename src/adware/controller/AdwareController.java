dbedba006625665354a6352b40a1a769f05530be
Waffesrgud <gcor8853@740s-pl-129.local>
2016-10-06 07:24:38 -0600

    empty for loop and a file with important stuff? What could possibly happen next

--- src/adware/controller/AdwareController.java
+++ src/adware/controller/AdwareController.java
@@ -1,18 +1,19 @@
 package adware.controller;
 
-import java.util.Scanner;
-
 import adware.view.AdwareView;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import adware.model.AdwareModel;
 
 public class AdwareController
 {
 	private AdwareView display;
-	public AdwareController()
-	{
-		display = new AdwareView();
-	}
+	private List<AdwareModel> thingyList;
+	private List<String> importantFiles;
 	
-	public void start()
+	
+public void start()
 	{
 		/* String computerBrand = "Toshiba PIX2";
 		int count = 0;
@@ -37,7 +38,8 @@ public class AdwareController
 		
 		
 		}	*/
-		String answer = display.collectResponse("Any number you input is fine, haha. Just make sure it's a number, haha.");
+		
+		/*String answer = display.collectResponse("Any number you input is fine, haha. Just make sure it's a number, haha.");
 		if(answer != null && !answer.equals(""))
 		{
 		if(isDouble(answer))
@@ -52,8 +54,10 @@ public class AdwareController
 			System.out.println("Shut up, nerd boy.");
 		}
 		
-		}
+		} */
+		learnLists();
 	}
+	
 	/**
 	 * This program checks if the input is a valid double and if otherwise returns a message indicating so.
 	 * @param potentialValue The input string.
@@ -75,6 +79,7 @@ public class AdwareController
 		
 		return isParseable;
 	}
+	
 	/**
 	 * Checks if the input is a valid integer and if otherwise returns a message indicating so.
 	 * @param potentialNumber The input string.
@@ -95,5 +100,78 @@ public class AdwareController
 			display.displayMessage("Type in an integer you friccin moron..!");			
 		}
 		return isParseable;
+	}
+
+	public AdwareController()
+	{
+		display = new AdwareView();
+		thingyList = new ArrayList<AdwareModel>();
+	}
+	private void testLoop()
+	{
+		String answer = "sample";
+		while(answer != null && !isDouble(answer))
+		{
+			answer = display.collectResponse("Type a number, bag.");
+		}
+	}
+	
+	private void learnLists()
+	{
+		/*display.displayMessage(thingyList.size() + " is the size of the list.");
+		AdwareModel testThingy = new AdwareModel();
+		thingyList.add(testThingy);
+		display.displayMessage(thingyList.size() + " is the size of the list.");
+		
+		 for (int index = 0; index <5; index++)
+		 {
+			  AdwareModel addThingy = new AdwareModel();
+			  thingyList.add(addThingy);
+			  display.displayMessage(addThingy.getWords());
+		 }
+		 
+		 display.displayMessage(thingyList.size() + " is the size of the list.");
+		 
+		 thingyList.get(3).setWords("enemy spotted, value changed");
+		 AdwareModel temp = thingyList.get(4);
+		 temp.setWords("stuff amirite");
+		 
+		 for (int index = 0; index < thingyList.size(); index++)
+		 {
+			 display.displayMessage(thingyList.get(index).getWords());
+		 }
+		 
+		 thingyList.add(3, new AdwareModel());
+		 
+		 display.displayMessage("The size is " + thingyList.size());
+		 
+		 thingyList.remove(0);
+		 
+		 display.displayMessage("The size is " + thingyList.size());
+		 
+		 AdwareModel deletedThingy = thingyList.remove(3);
+		 display.displayMessage(deletedThingy.getWords() + " and the list size is now " + thingyList.size());
+		 
+		 thingyList.set(0,  deletedThingy);
+		 
+		 for (int index = 0; index < thingyList.size(); index++){
+			 display.displayMessage(thingyList.get(index).getWords());
+		 }
+		 
+		 for (AdwareModel currentThingy : thingyList)
+		 {
+			 display.displayMessage(currentThingy.getWords());
+			 thingyList.remove(0);
+		 }
+		 */
+		 for (int index = 0; index < 8; index++){
+			 importantFiles.add("This is a very important file. DO NOT DELETE!");
+		 }
+		 
+		 for (int index = 0; index < importantFiles.size(); index++){
+			 
+		 }
+		 
+		 
 	}
 }
